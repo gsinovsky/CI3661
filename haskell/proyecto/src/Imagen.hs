2da7cf2e54b52f2cc0d54479d4442c4d9bef562e
Gustavo Siñovsky <g.sinovsky@gmail.com>
2015-05-07 14:08:43 -0430

    Ready subImagen

--- haskell/proyecto/src/Imagen.hs
+++ haskell/proyecto/src/Imagen.hs
@@ -2,11 +2,12 @@ module Imagen
   ( hSplit, vSplit
   , colorPromedio
   , subImagen
+  , getDatos
   )
   where
 
+import Data.List (genericTake, genericDrop)
 import Graphics.Mosaico.Imagen (Color(Color, rojo, verde, azul), Imagen(Imagen, altura, anchura, datos))
-
 
 
 subImagen
@@ -16,8 +17,11 @@ subImagen
 
 subImagen xInicial yInicial anchura' altura' imagen
      = Imagen anchura' altura' (subLista (datos imagen))
-     where subLista = take (fromIntegral altura') . drop (fromIntegral xInicial) 
+     where subLista = map f . genericTake altura' . genericDrop xInicial;
+           f = genericTake anchura' . genericDrop yInicial
 
+getDatos :: Imagen -> [[Color]]
+getDatos (Imagen _ _ m) = m
 
 hSplit :: Imagen -> (Imagen, Imagen)
 hSplit = undefined
