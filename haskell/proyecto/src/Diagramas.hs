3bc4119366ac56f0bb6097701615e8de5fa49ae3
Gustavo Siñovsky <g.sinovsky@gmail.com>
2015-05-13 17:17:41 -0430

    caminar and dividir

--- haskell/proyecto/src/Diagramas.hs
+++ haskell/proyecto/src/Diagramas.hs
@@ -24,10 +24,25 @@ data Orientación
   deriving Show
 
 dividir :: Orientación -> Rectángulo -> Maybe Diagrama
-dividir = undefined
+dividir Horizontal (Rectángulo _ imagen') = if anchura imagen' >= 2 
+  then Just $ Hoja (rectánguloImagen superior) :-: Hoja (rectánguloImagen inferior)
+  else Nothing
+  where (superior, inferior) = hSplit imagen'
+dividir Vertical (Rectángulo _ imagen') = if altura imagen' >= 2 
+  then Just $ Hoja (rectánguloImagen izquierda) :|: Hoja (rectánguloImagen derecha)
+  else Nothing
+  where (izquierda, derecha) = vSplit imagen'
 
 caminar :: [Paso] -> Diagrama -> Maybe Diagrama
-caminar = undefined
+caminar [] d = Just d
+caminar (_:_) (Hoja _) = Nothing
+caminar (Primero:xs) (p :-: _) = caminar xs p
+caminar (Primero:xs) (p :|: _) = caminar xs p
+caminar (Segundo:xs) (_ :-: s) = caminar xs s
+caminar (Segundo:xs) (_ :|: s) = caminar xs s
 
-sustituir :: Diagrama -> [Paso] -> Diagrama -> Diagrama
+
+
+sustituir :: Diagrama -> [Paso] -> Diagrama -> Maybe Diagrama
 sustituir = undefined
+
